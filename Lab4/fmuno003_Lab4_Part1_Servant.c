851a857dd524500dcd278422b6e0eb3c76e3211e
Francisco Munoz <fmuno003@ucr.edu>
2016-10-12 16:55:17 -0700

    Update fmuno003_Lab4_Part1_Servant.c

--- Lab4/fmuno003_Lab4_Part1_Servant.c
+++ Lab4/fmuno003_Lab4_Part1_Servant.c
@@ -1,7 +1,7 @@
 /*	Partner(s) Name & E-mail: Francisco Munoz	fmuno003@ucr.edu
  *	Lab Section: 022
  *	Assignment: Lab #4  Exercise #1
- *	Exercise Description:
+ *	Exercise Description: Servant
  *	
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -28,7 +28,6 @@
 int count = 0xFF;
 unsigned char receivedData;
 enum servantStates {s_wait, read} states;
-enum masterStates {wait, send} state;
 
 void transmit_data(unsigned char data) {
 	unsigned char i;
@@ -41,30 +40,10 @@ void transmit_data(unsigned char data) {
 		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
 		PORTC |= 0x04;
 	}
-	// set RCLK = 1. Rising edge copies data from the “Shift” register to the “Storage” register
+	// set RCLK = 1. Rising edge copies data from the â€œShiftâ€ register to the â€œStorageâ€ register
 	PORTC |= 0x02;
 	// clears all lines in preparation of a new transmission
 	PORTC = 0x00;
-}
-void SPI_MasterInit(void) {
-	// Set DDRB to have MOSI, SCK, and SS as output and MISO as input
-	DDRB = 0xBF; PORTB = 0x40;
-	// Set SPCR register to enable SPI, enable master, and use SCK frequency
-	SPCR |= (1<<SPE) | (1<<MSTR) | (1<<SPR0);
-	//   of fosc/16  (pg. 168)
-	// Make sure global interrupts are enabled on SREG register (pg. 9)
-	SREG =0x80;
-}
-void SPI_MasterTransmit(unsigned char cData) {
-	// set SS low
-	PORTB = SetBit(PORTB,4,0);
-	// data in SPDR will be transmitted, e.g. SPDR = cData;
-	SPDR = cData;
-	while(!(SPSR & (1<<SPIF))) { // wait for transmission to complete
-		;
-	}
-	// set SS high
-	PORTB = SetBit(PORTB,4,1);
 }
 void SPI_ServantInit(void) {
 	// set DDRB to have MISO line as output and MOSI, SCK, and SS as input
@@ -73,30 +52,6 @@ void SPI_ServantInit(void) {
 	SPCR |= (1<<SPE) | (1<<SPIE);
 	// make sure global interrupts are enabled on SREG register (pg. 9)
 	SREG = 0x80;
-}
-void TickFct_master() 
-{
-	switch( state ) 
-	{
-		case wait:
-			state = send;
-			break;
-		case send:
-			state = wait;
-			break;
-		default:
-			break;
-	}
-	switch( state ) 
-	{
-		case wait:
-			break;
-		case send:
-			SPI_MasterTransmit(count);
-			break;
-		default:
-			break;
-	}
 }
 void TickFct_servant() 
 {
@@ -138,18 +93,12 @@ void StartSecPulse(unsigned portBASE_TYPE Priority)
 }
 int main(void)
 {
-        // MASTER
-        //DDRC = 0xF0; PORTC = 0x0F;
-        //DDRD = 0xFF; PORTD = 0x00;
-        //DDRA = 0xFF; PORTA = 0x00;
-        //SPI_MasterInit();
-        
         //SLAVE
         SPI_ServantInit();
         DDRC = 0xFF; PORTC = 0x00;
         DDRA = 0xFF; PORTA = 0x00;
-		StartSecPulse(1);
-		vTaskStartScheduler();
+	StartSecPulse(1);
+	vTaskStartScheduler();
 		
 	return 0;       
 }
