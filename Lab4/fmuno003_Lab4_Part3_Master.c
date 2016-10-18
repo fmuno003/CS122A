bdeb37c6ee9deb4b5f52367634baf111b3c3fcc4
Francisco Munoz <fmuno003@ucr.edu>
2016-10-17 19:32:19 -0700

    Update fmuno003_Lab4_Part3_Master.c

--- Lab4/fmuno003_Lab4_Part3_Master.c
+++ Lab4/fmuno003_Lab4_Part3_Master.c
@@ -1,53 +1,26 @@
 /*	Partner(s) Name & E-mail: Francisco Munoz	fmuno003@ucr.edu
  *	Lab Section: 022
- *	Assignment: Lab #4  Exercise #2
+ *	Assignment: Lab #4  Exercise #3
  *	Exercise Description: Master
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */ 
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <math.h>
-#include <avr/interrupt.h>
 #include <avr/eeprom.h>
-#include <avr/portpins.h>
-#include <avr/pgmspace.h>
-
 //FreeRTOS include files
 #include "FreeRTOS.h"
 #include "task.h"
 #include "croutine.h"
-#include "bit.h"
 #include "keypad.h"
 #include "lcd.h"
 
-unsigned char btn;
-char array[32] = "Ptrn: 1 Spd: 1  uC: 1";
-unsigned char data = 0x00;
-enum masterStates {m_read} state;
+char array[22] = "Ptrn: 1 Spd: 1  uC: 1";
+unsigned char clr_highNibble = 0x0F;
+unsigned char clr_lowNibble = 0xF0;
+unsigned char data, btn;
 
-void transmit_data(unsigned char data) 
-{
-	unsigned char i;
-	for (i = 0; i < 8 ; ++i) {
-		// Sets SRCLR to 1 allowing data to be set
-		// Also clears SRCLK in preparation of sending data
-		PORTC = 0x08;
-		// set SER = next bit of data to be sent.
-		PORTC |= ((data >> i) & 0x01);
-		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
-		PORTC |= 0x04;
-	}
-	// set RCLK = 1. Rising edge copies data from the “Shift” register to the “Storage” register
-	PORTC |= 0x02;
-	// clears all lines in preparation of a new transmission
-	PORTC = 0x00;
-}	
+enum masterStates {m_read} state;
 void SPI_MasterInit(void) {
 	// Set DDRB to have MOSI, SCK, and SS as output and MISO as input
 	DDRB = 0xBF; PORTB = 0x40;
@@ -84,54 +57,63 @@ void TickFct_master()
 		case m_read:
 			btn = GetKeypadKey();
 		switch( btn ) 
-		{
-			case 'A':
-				data = data | 0x10;
-				array[6] = '1';				
-				break;
-			case 'B':
-				data = data | 0x20;
-				array[6] = '2';
-				break;
-			case 'C':
-				data = data | 0x30;
-				array[6] = '3';
-				break;
-			case 'D':
-				data = data | 0x40;
-				array[6] = '4';
-				break;
-			case '1':
-				data = data | 0x01;
-				array[13] = '1';
-				break;
-			case '2':
-				data = data | 0x02;
-				array[13] = '2';
-				break;
-			case '3':
-				data = data | 0x03;
-				array[13] = '3';
-				break;
-			case '4':
-				data = data | 0x04;
-				array[13] = '4';
-				break;
-			case '5':
-				data = data | 0x05;
-				array[13] = '5';
-				break;
-			case '6':
-				data = data | 0x06;
-				array[13] = '6';
-				break;
-			default:
-				break;
-		}
-		SPI_MasterTransmit(data);
-		LCD_Cursor(1);
-		LCD_DisplayString(1, array);
-		break;
+			{
+				case 'A':
+					data = data & clr_highNibble;
+					data = data | 0x10;
+					array[6] = '1';
+					break;
+				case 'B':
+					data = data & clr_highNibble;
+					data = data | 0x20;
+					array[6] = '2';
+					break;
+				case 'C':
+					data = data & clr_highNibble;
+					data = data | 0x30;
+					array[6] = '3';
+					break;
+				case 'D':
+					data = data & clr_highNibble;
+					data = data | 0x40;
+					array[6] = '4';
+					break;
+				case '1':
+					data = data & clr_lowNibble;
+					data = data | 0x01;
+					array[13] = '1';
+					break;
+				case '2':
+					data = data & clr_lowNibble;
+					data = data | 0x02;
+					array[13] = '2';
+					break;
+				case '3':
+					data = data & clr_lowNibble;
+					data = data | 0x03;
+					array[13] = '3';
+					break;
+				case '4':
+					data = data & clr_lowNibble;
+					data = data | 0x04;
+					array[13] = '4';
+					break;
+				case '5':
+					data = data & clr_lowNibble;
+					data = data | 0x05;
+					array[13] = '5';
+					break;
+				case '6':
+					data = data & clr_lowNibble;
+					data = data | 0x06;
+					array[13] = '6';
+					break;
+				default:
+					break;
+			}
+			SPI_MasterTransmit(data);
+			LCD_DisplayString(1, array);
+			break;
 		default:
 			break;
 	}
@@ -142,7 +124,8 @@ void Master_Task()
 	for(;;)
 	{
 		TickFct_master();
-		vTaskDelay(500);
+		vTaskDelay(250);
+		
 	}
 }
 void StartSecPulse(unsigned portBASE_TYPE Priority)
@@ -153,12 +136,12 @@ int main(void)
 {
         // MASTER
         DDRC = 0xF0; PORTC = 0x0F;
-        DDRD = 0xFF; PORTD = 0x00;
-        DDRA = 0xFF; PORTA = 0x00;
+		DDRA = 0xFF; PORTD = 0x00;
+		DDRD = 0xFF; PORTD = 0x00;
 		LCD_init();
-		//LCD_DisplayString(1, "Ptrn:");
         SPI_MasterInit();
 		StartSecPulse(1);
 		vTaskStartScheduler();
+		
 	return 0;       
 }
