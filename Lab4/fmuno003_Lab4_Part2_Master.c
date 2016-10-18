49ff7443403f66de1b22e691987db43e8861ffac
Francisco Munoz <fmuno003@ucr.edu>
2016-10-17 18:48:44 -0700

    Update fmuno003_Lab4_Part2_Master.c

--- Lab4/fmuno003_Lab4_Part2_Master.c
+++ Lab4/fmuno003_Lab4_Part2_Master.c
@@ -7,50 +7,18 @@
  *	code, is my own original work.
  */ 
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <math.h>
-#include <avr/interrupt.h>
 #include <avr/eeprom.h>
-#include <avr/portpins.h>
-#include <avr/pgmspace.h>
-
 //FreeRTOS include files
 #include "FreeRTOS.h"
 #include "task.h"
 #include "croutine.h"
-#include "bit.h"
 #include "keypad.h"
-#include "lcd.h"
 
-unsigned char receivedData;
 unsigned char clr_highNibble = 0x0F;
 unsigned char clr_lowNibble = 0xF0;
-unsigned char btn;
-unsigned char data = 0x11;
+unsigned char data, btn;
 
 enum masterStates {m_read} state;
-
-void transmit_data(unsigned char data) 
-{
-	unsigned char i;
-	for (i = 0; i < 8 ; ++i) {
-		// Sets SRCLR to 1 allowing data to be set
-		// Also clears SRCLK in preparation of sending data
-		PORTC = 0x08;
-		// set SER = next bit of data to be sent.
-		PORTC |= ((data >> i) & 0x01);
-		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
-		PORTC |= 0x04;
-	}
-	// set RCLK = 1. Rising edge copies data from the “Shift” register to the “Storage” register
-	PORTC |= 0x02;
-	// clears all lines in preparation of a new transmission
-	PORTC = 0x00;
-}	
 void SPI_MasterInit(void) {
 	// Set DDRB to have MOSI, SCK, and SS as output and MISO as input
 	DDRB = 0xBF; PORTB = 0x40;
@@ -72,12 +40,6 @@ void SPI_MasterTransmit(unsigned char cData)
 	// set SS high
 	PORTB = SetBit(PORTB,4,1);
 }
-ISR(SPI_STC_vect){ // this is enabled in with the SPCR register?s ?SPI
-	// Interrupt Enable?
-	// SPDR contains the received data, e.g. unsigned char receivedData =
-	// SPDR;
-	receivedData = SPDR;
-}
 void TickFct_master() 
 {
 	switch( state ) 
@@ -93,52 +55,52 @@ void TickFct_master()
 		case m_read:
 			btn = GetKeypadKey();
 		switch( btn ) 
-		{
-			case 'A':
-				data = data & clr_highNibble;
-				data = data | 0x10;
-				break;
-			case 'B':
-				data = data & clr_highNibble;
-				data = data | 0x20;
-				break;
-			case 'C':
-				data = data & clr_highNibble;
-				data = data | 0x30;
-				break;
-			case 'D':
-				data = data & clr_highNibble;
-				data = data | 0x40;
-				break;
-			case '1':
-				data = data & clr_lowNibble;
-				data = data | 0x01;
-				break;
-			case '2':
-				data = data & clr_lowNibble;
-				data = data | 0x02;
-				break;
-			case '3':
-				data = data & clr_lowNibble;
-				data = data | 0x03;
-				break;
-			case '4':
-				data = data & clr_lowNibble;
-				data = data | 0x04;
-				break;
-			case '5':
-				data = data & clr_lowNibble;
-				data = data | 0x05;
-				break;
-			case '6':
-				data = data & clr_lowNibble;
-				data = data | 0x06;
-				break;
-			default:
-				break;
-		}
-		SPI_MasterTransmit(data);
-		break;
+			{
+				case 'A':
+					data = data & clr_highNibble;
+					data = data | 0x10;
+					break;
+				case 'B':
+					data = data & clr_highNibble;
+					data = data | 0x20;
+					break;
+				case 'C':
+					data = data & clr_highNibble;
+					data = data | 0x30;
+					break;
+				case 'D':
+					data = data & clr_highNibble;
+					data = data | 0x40;
+					break;
+				case '1':
+					data = data & clr_lowNibble;
+					data = data | 0x01;
+					break;
+				case '2':
+					data = data & clr_lowNibble;
+					data = data | 0x02;
+					break;
+				case '3':
+					data = data & clr_lowNibble;
+					data = data | 0x03;
+					break;
+				case '4':
+					data = data & clr_lowNibble;
+					data = data | 0x04;
+					break;
+				case '5':
+					data = data & clr_lowNibble;
+					data = data | 0x05;
+					break;
+				case '6':
+					data = data & clr_lowNibble;
+					data = data | 0x06;
+					break;
+				default:
+					break;
+			}
+			SPI_MasterTransmit(data);
+			break;
 		default:
 			break;
 	}
@@ -149,6 +111,7 @@ void Master_Task()
 	for(;;)
 	{
 		TickFct_master();
+		vTaskDelay(200);
 	}
 }
 void StartSecPulse(unsigned portBASE_TYPE Priority)
@@ -159,8 +122,6 @@ int main(void)
 {
         // MASTER
         DDRC = 0xF0; PORTC = 0x0F;
-        DDRD = 0xFF; PORTD = 0x00;
-        DDRA = 0xFF; PORTA = 0x00;
         SPI_MasterInit();
 		StartSecPulse(1);
 		vTaskStartScheduler();
