693f932db1b01d24b6c953eed64f6f1f5b0f4d85
Francisco Munoz <fmuno003@ucr.edu>
2016-10-18 20:56:04 -0700

    Update fmuno003_Lab5_Part2.c

--- Lab5/fmuno003_Lab5_Part2.c
+++ Lab5/fmuno003_Lab5_Part2.c
@@ -24,13 +24,20 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "croutine.h"
-#include "usart_ATmega1284.h"
+#include "bit.h"
 
-void transmit_data(unsigned char data) 
+unsigned int i = 0;
+unsigned char pattern = 0x01;
+int power = 0;
+enum Pattern_States{init1, on} state;
+enum Pattern_States2{init2, on2, off2} states;
+enum Pattern_States3{init3, on3, off3} state1;
+enum Button_Press{init, up, down, off, wait} btnState;
+void transmit_data(unsigned char data)
 {
 	unsigned char i;
-	for (i = 0; i < 8 ; ++i) 
-  {
+	for (i = 0; i < 8 ; ++i)
+	{
 		// Sets SRCLR to 1 allowing data to be set
 		// Also clears SRCLK in preparation of sending data
 		PORTC = 0x08;
@@ -44,89 +51,93 @@ void transmit_data(unsigned char data)
 	// clears all lines in preparation of a new transmission
 	PORTC = 0x00;
 }
-int power = 0;
-enum Pattern_States{init, on} state;
-enum Pattern_States2{init2, on} states;
-enum Pattern_States3{iinit3, on} state1;
-enum Button_Press{init, up, down, off} State;
-
 void Tick_Button()
 {
-	switch(State)
+	switch(btnState)
 	{
 		case init:
-			if(power && (~PINA & 0x01 == 0x01))
-			{
-				State = up;
-			}
-			else if(power && (~PINA & 0x02 == 0x02))
-			{
-				State = down;
-			}
-			else if(power && (~PINA & 0x03 == 0x03))
-			{
-				State = off;
-			}
+			if(power && GetBit(PINA, 0))
+				btnState = up;
+			else if(power && GetBit(PINA, 1))
+				btnState = down;
+			else if(power && GetBit(PINA, 1) && GetBit(PINA, 0))
+				btnState = off;
 			else
-			{
-				State = init;
-			}
+				btnState = init;
 			break;
 		case up:
-			if(power && (~PINA & 0x01 == 0x01))
-			{
-				State = wait;
-			}
+			if(power && GetBit(PINA, 0))
+				btnState = wait;
 			break;
 		case down:
-			if(power && (~PINA & 0x02 == 0x02))
-			{
-				State = wait;
-			}
+			if(power && GetBit(PINA, 1))
+				btnState = wait;
 			break;
 		case off:
-			if(~PINA & 0x03 == 0x03)
-			{
-				State = powerOff;
-			}
+			if(GetBit(PINA, 0) && GetBit(PINA, 1))
+				btnState = off;
 			break;
 		case wait:
-			if(~PINA & 0x03 == 0x00)
-			{
-				State = init;
-			}
+			if(GetBit(PINA, 0) && GetBit(PINA, 1))
+				btnState = init;
 			break;
-		case powerOff:
-			if(!power && (~PINA & 0x03 == 0x03))
-			{
-				State = init;
-			}
+		case off:
+			if(!power && GetBit(PINA, 1) && GetBit(PINA, 0))
+				btnState = init;
 			break;
 		default:
 			break;
 	}
-	
+	switch(btnState)
+	{
+		case init:
+			break;
+		case up:
+			if(i < 4)
+				++i;
+			break;
+		case down:
+			if(i > 0)
+				--i;
+			break;
+		case off:
+			power = !power;
+			break;
+		case wait;
+			break;
+		default:
+			break;
+	}
+}
 void Tick_Pattern1()
 {
 	switch(state)
 	{
-    	case init:
-     		state = on;
+    	case init1:
+			if(i == 0)
+     			state = on;
     		break;
     	case on:
-			state = on;
+			if(i != 0)
+				state = init1;
+			else
+				state = on;
 			break;
 		default:
 			break;
 	}
 	switch(state)
 	{
-		case init:
+		case init1:
 			break;
 		case on:
-			for(i = 0; i < 256; ++i)
+			for(int j = 0; j < 256; ++j)
 			{
-				transmit_data(array[i]);
+				transmit_data(j);
+			}
+			for(int j = 255; j > 0; --j)
+			{
+				transmit_data(j);
 			}
 			break;
 		default:
@@ -138,11 +149,20 @@ void Tick_Pattern2()
 	switch(states)
 	{
 		case init2:
-			state = on2;
+			if(i == 1)
+				states = on2;
 			break;
 		case on2:
-			state = on2;
+			if(i != 1)
+				states = init2;
+			else
+				states = off2;
 			break;
+		case off2:
+			if(i != 1)
+				states = init2;
+			else
+				states = on2;
 		default:
 			break;
 	}
@@ -151,10 +171,10 @@ void Tick_Pattern2()
 		case init2:
 			break;
 		case on2:
-			for(j = 0; j < 30; ++j)
-			{
-				transmit_data(arrays[j]);
-			}
+			transmit_data(0xAA);
+			break;
+		case off2:
+			transmit_data(0x55);
 			break;
 		default:
 			break;
@@ -168,7 +188,20 @@ void Tick_Pattern3()
 			state1 = on3;
 			break;
 		case on3:
-			state1 = on3;
+			if(i != 2)
+				state1 = init3;
+			else if(i == 2 && pattern != 0x80)
+				state1 = on3;
+			else if(i == 2 && pattern == 0x80)
+				state1 = off3;
+			break;
+		case off3:
+			if(i != 2)
+				state1 = init3;
+			else if(i == 2 && pattern != 0x01)
+				state1 = on3;
+			else if(i == 2 && pattern == 0x01)
+				state1 = off3;
 			break;
 		default:
 			break;
@@ -178,12 +211,78 @@ void Tick_Pattern3()
 		case init3:
 			break;
 		case on3:
-			for(k = 0; k < 15; ++k)
-			{
-				transmit_data(patternArray[k]);
-			}
+			pattern = pattern << 1;
+			transmit_data(pattern);
+			break;
+		case off3:
+			pattern = pattern >> 1;
+			transmit_data(pattern);
 			break;
 		default:
 			break;
 	}
+}
+void Shift_Task()
+{
+	btnState = init;
+	for(;;)
+	{
+		Tick_Button();
+		vTaskDelay(250);
+	}
+}
+void Pattern1_Task()
+{
+	state = init1;
+	for(;;)
+	{
+		Tick_Pattern1();
+		vTaskDelay(250);
+	}
+}
+void Pattern2_Task()
+{
+	states = init2;
+	for(;;)
+	{
+		Tick_Pattern2();
+		vTaskDelay(250);
+	}
+}
+void Pattern3_Task()
+{
+	state1 = init3;
+	for(;;)
+	{
+		Tick_Pattern3();
+		vTaskDelay(250);
+	}
+}
+void StartShiftPulse(unsigned portBASE_TYPE Priority)
+{
+	xTaskCreate(Shift_Task, (signed portCHAR *)"Shift_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
+}
+void StartPatternPulse(unsigned portBASE_TYPE Priority)
+{
+	xTaskCreate(Pattern1_Task, (signed portCHAR *)"Pattern1_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
+}
+void StartPattern1Pulse(unsigned portBASE_TYPE Priority)
+{
+	xTaskCreate(Pattern2_Task, (signed portCHAR *)"Pattern2_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
+}
+void StartPattern2Pulse(unsigned portBASE_TYPE Priority)
+{
+	xTaskCreate(Pattern3_Task, (signed portCHAR *)"Pattern3_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
+}
+int main()
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	
+	StartShiftPulse(1);
+	StartPatternPulse(1);
+	StartPattern1Pulse(1);
+	StartPattern2Pulse(1);
+	vTaskStartScheduler();
+	return 0;
 }
