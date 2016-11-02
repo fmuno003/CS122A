3d2e910a3e3fe89ea242da220fe0b441ee21315d
Francisco Munoz <fmuno003@ucr.edu>
2016-11-02 12:21:27 -0700

    Update fmuno003_Lab7_Part4.c

--- Lab7/fmuno003_Lab7_Part4.c
+++ Lab7/fmuno003_Lab7_Part4.c
@@ -13,10 +13,11 @@
 #include "task.h"
 #include "croutine.h"
 
-unsigned char pattern = 0x00;
-unsigned char row = 0xF5;
-enum LRStates{init, left, right} LRstate;
-enum UDStates{wait, up, down} UDstate;
+unsigned char pattern = 0x01;
+unsigned char row = 0x10;
+unsigned short joystick = 0;
+unsigned short temp = 0;
+enum LRStates{leftRight, upDown} state;
 enum LEDMatrixStates{display} LEDstate;
 
 
@@ -38,29 +39,11 @@ void A2D_init()
 	//ADSC: Starts analog-to-digital conversion
 	//ADATE: Enables auto-triggering, allowing for constant analog to digital conversions.
 }
-void transmit_data(unsigned char data)
+void convert()
 {
-	unsigned char i;
-	for(i = 0; i < 8; ++i)
-	{
-		PORTC = 0x08;
-		PORTC |= ((data >> i) & 0x01);
-		PORTC |= 0x04;
-	}
-	PORTC |= 0x02;
-	PORTC = 0x00;
-}
-void transmit_data1(unsigned char data)
-{
-	unsigned char i;
-	for(i = 0; i < 8; ++i)
-	{
-		PORTD = 0x08;
-		PORTD |= ((data >> i) & 0x01);
-		PORTD |= 0x04;
-	}
-	PORTD |= 0x02;
-	PORTD = 0x00;
+	ADCSRA |=(1<<ADSC);//start ADC conversion
+	while ( !(ADCSRA & (1<<ADIF)));//wait till ADC conversion
+	
 }
 void LEDMatrix_Tick()
 {
@@ -75,8 +58,8 @@ void LEDMatrix_Tick()
 	{
 		case display:
 
-			transmit_data(pattern);
-			transmit_data1(row);
+			PORTD = pattern;
+			PORTC = ~row;
 			break;
 		default:
 			break;
@@ -84,87 +67,42 @@ void LEDMatrix_Tick()
 }
 void LR_Tick()
 {
-	switch(LRstate)
+	switch(state)
 	{
-		case init:
-			if(ADC > 700)
-				LRstate = right;
-			else if(ADC < 350)
-				LRstate = left;
-			else
-				LRstate = init;
+		case leftRight:
+			Set_A2D_Pin(0x00);
+			convert();
+			joystick = ADC;
+			if(joystick < 350)
+			{
+				if(pattern != 0x01)
+					pattern = pattern >> 1;
+			}
+			else if(joystick > 700)
+			{
+				if(pattern != 0x80)
+					pattern = pattern << 1;
+			}
+			state = upDown;
 			break;
-		case left:
-			if(ADC < 350)
-				LRstate = left;
-			else
-				LRstate = init;
-			break;
-		case right:
-			if(ADC > 700)
-				LRstate = right;
-			else
-				LRstate = init;
+		case upDown:
+			Set_A2D_Pin(0x01);
+			convert();
+			temp = ADC;
+			if(temp < 350)
+			{
+				if(row != 0x10)
+					row = row << 1;
+			}
+			else if(temp > 700)
+			{
+				if(row != 0x01)
+					row = row >> 1;
+			}
+			state = leftRight;
 			break;
 		default:
 			break;
-	}
-	switch(LRstate)
-	{
-		case init:
-			break;
-		case left:
-			if(pattern != 0x80)
-				pattern = pattern << 1;
-			break;
-		case right:
-			if(pattern != 0x01)
-				pattern = pattern >> 1;
-			break;
-		default:
-			break;
-	}
-}
-void UD_Tick()
-{
-	switch(UDstate)
-	{
-		case wait:
-			if(ADC > 700)
-				UDstate = up;
-			else if(ADC < 350)
-				UDstate = down;
-			else
-				UDstate = wait;
-			break;
-		case up:
-			if(ADC > 700)
-				UDstate = up;
-			else
-				UDstate = wait;
-			break;
-		case down:
-			if(ADC < 350)
-				UDstate = down;
-			else
-				UDstate = wait;
-			break;
-		default:
-			break;
-	}
-	switch(UDstate)
-	{
-		case wait:
-			break;
-		case up:
-			if(row != 0xFE)
-				row = row << 1;
-			break;
-		case down:
-			if(row != 0xE0)
-				row = row >> 1;
-			break;
-		
 	}
 }
 void LEDMatrix_Task()
@@ -176,18 +114,9 @@ void LEDMatrix_Task()
 		vTaskDelay(250);
 	}
 }
-void UD_Task()
-{
-	UDstate = wait;
-	for(;;)
-	{
-		UD_Tick();
-		vTaskDelay(250);
-	}
-}
 void LR_Task()
 {
-	LRstate = init;
+	state = leftRight;
 	for(;;)
 	{
 		LR_Tick();
@@ -196,14 +125,13 @@ void LR_Task()
 }
 void StartShiftPulse(unsigned portBASE_TYPE Priority)
 {
-	xTaskCreate(UD_Task, (signed portCHAR *) "UD_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
-	xTaskCreate(LEDMatrix_Task, (signed portCHAR *) "LEDMatrix_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
 	xTaskCreate(LR_Task, (signed portCHAR *) "LR_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
+	xTaskCreate(LEDMatrix_Task, (signed portCHAR *) "LEDMatrix_Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL);
 }
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
 	A2D_init();
 	StartShiftPulse(1);
